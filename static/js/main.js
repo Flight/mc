4a7d6d2c555193dc55780a91cff9edfb90b4a352
Flight <flight.neg@gmail.com>
2017-02-14 23:05:26 +0200

    Main page and login improvements

--- static/js/main.js
+++ static/js/main.js
@@ -2,4 +2,28 @@
     $('.js-slick').slicknav({
         label: ''
     });
+
+
 } (jQuery));
+
+$(window).on('load scroll resize', positionFooter);
+
+function positionFooter() {
+    var $window = $(window),
+        $body = $('body'),
+        $footer = $('.footer'),
+        footerHeight = $footer.height(),
+        footerTop = $window.scrollTop() + $window.height() - footerHeight;
+
+    if ($body.height() + footerHeight < $window.height()) {
+        $footer.css({
+            position: 'absolute',
+            top: footerTop + 'px'
+        });
+    } else {
+        $footer.css({
+           position: 'static'
+        });
+    }
+
+}
