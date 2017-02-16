e1e67631dee7a8d0a212aa3c9c94cdfee9059b95
Flight <flight.neg@gmail.com>
2017-02-16 22:23:47 +0200

    login fixes, js errors fixes

--- static/js/main.js
+++ static/js/main.js
@@ -1,29 +1,28 @@
 (function ($) {
-    $('.js-slick').slicknav({
-        label: ''
+    $(function() {
+        $('.js-slick').slicknav({
+            label: ''
+        });
     });
 
+    $(window).on('load scroll resize', positionFooter);
 
-} (jQuery));
+    function positionFooter() {
+        var $window = $(window);
+        var $body = $('body');
+        var $footer = $('.footer');
+        var footerHeight = $footer.height();
+        var footerTop = $window.scrollTop() + $window.height() - footerHeight;
 
-$(window).on('load scroll resize', positionFooter);
-
-function positionFooter() {
-    var $window = $(window),
-        $body = $('body'),
-        $footer = $('.footer'),
-        footerHeight = $footer.height(),
-        footerTop = $window.scrollTop() + $window.height() - footerHeight;
-
-    if ($body.height() + footerHeight < $window.height()) {
-        $footer.css({
-            position: 'absolute',
-            top: footerTop + 'px'
-        });
-    } else {
-        $footer.css({
-           position: 'static'
-        });
+        if ($body.height() + footerHeight < $window.height()) {
+            $footer.css({
+                position: 'absolute',
+                top: footerTop + 'px'
+            });
+        } else {
+            $footer.css({
+                position: 'static'
+            });
+        }
     }
-
-}
+} (jQuery));
